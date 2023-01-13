239fb06ae7a35a7a9d05ffbd6b08d4fffe21c73e
Ezekiel <zopeaj@gmail.com>
2023-01-13 08:34:46 +0100

    account/models.py commited

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    id = models.PrimaryKeyField(primary_key=True, defaults=random_uuid)
+    name = models.CharField(max_length=255)
+
